4624320d144d0df71072e62eca7b26342230e630
7om-Balancist <shunhua7276@gmail.com>
2026-07-13 20:24:49 +0800

    Delete MSPM0G3507/Liner_Car0_Tom directory

--- MSPM0G3507/Liner_Car0_Tom/delay.c
+++
@@ -1,9 +0,0 @@
-#include "delay.h"
-
-#define SYS_FREQ_HZ 32000000U
-
-void delay_ms(uint32_t ms)
-{
-    uint32_t tick = ms * (SYS_FREQ_HZ / 4000);
-    for (volatile uint32_t i = 0; i < tick; ) i++;
-}
